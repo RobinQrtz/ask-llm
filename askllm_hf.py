c15500895e3c00ae4c0cad431dff1a209b1fe3e4
Robin Kurtz <robin.kurtz@kb.se>
2024-09-06 13:30:25 +0200

    askllm hacking

--- askllm_hf.py
+++ askllm_hf.py
@@ -1,103 +1,104 @@
 from argparse import ArgumentParser
 import torch
-import pandas as pd
 from torch.nn.functional import softmax
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset, Dataset, load_from_disk
 from functools import partial
 from tqdm import tqdm
-from ask_llm.prompt import apply_prompt_template
+from ask_llm.prompt import apply_askllm_prompt
+from ask_llm.askllm_dataset import AskLLMDataset
+import logging
 
 args = ArgumentParser()
-args.add_argument("--model_name", type=str, default="models/Mistral-7B-Instruct-v0.2t")
-args.add_argument("--token", type=str)
+args.add_argument(
+    "--model_name", type=str, default="models/Meta-Llama-3.1-70B-Instruct"
+)
+args.add_argument("--language", type=str, default="Swedish")
+args.add_argument("--data_shard", type=int, default=0)
+args.add_argument("--num_samples", type=int, default=40)
+args.add_argument("--batch_size", type=int, default=8)
+args.add_argument("--max_tokens", type=int, default=512)
+args.add_argument("--cache_dir", type=str, default="models/cache/")
+args.add_argument("--output_dir", type=str, default="output/")
+
 args = args.parse_args()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info(args)
 
 device = "cuda"  # the device to load the model onto
+yes_ids = [7566, 9642, 9891, 10035]  # llama yes ids
 
 model = AutoModelForCausalLM.from_pretrained(
     args.model_name,
     torch_dtype=torch.bfloat16,
     device_map="auto",
     attn_implementation="flash_attention_2",
-    cache_dir="models/mistral-7b",
-    token=args.token,
+    cache_dir=args.cache_dir,
 )
 
-tokenizer = AutoTokenizer.from_pretrained(args.model_name, padding_side="left", token=args.token)
+tokenizer = AutoTokenizer.from_pretrained(args.model_name, padding_side="left")
 
 tokenizer.pad_token = tokenizer.eos_token
 model.config.pad_token_id = model.config.eos_token_id
 
 # Load the dataset in streaming mode
-dataset = load_from_disk("culturaxlocal")
 # dataset = load_dataset("wikipedia", language="sv", date="20240520", streaming=True, cache_dir="wiki-sv",)
-
-
-def gen_from_iterable_dataset(iterable_ds):
-    yield from iterable_ds
-
-
-# ds = dataset["train"].take(2000)
-
-ds = dataset
-# Convert iterabledataset to dataset1
-ds = Dataset.from_generator(partial(gen_from_iterable_dataset, ds), features=ds.features)
-ds = ds.map(
-    apply_prompt_template, batched=False, fn_kwargs={"tokenizer": tokenizer, "max_length": 450}
+dataset = load_dataset(
+    "json",
+    data_files={"train": args.input_file},
+    split="train",
+    cache_dir="data/cache_dir",
 )
 
+logging.info(f"Shuffling dataset")
+# should shuffle before take
+dataset = dataset.shuffle(seed=666)
+ds = dataset.take(args.num_samples)
 
-def word_len(example):
-    example["word_count"] = len(example["text"].split(" "))
-    return example
+# # compute ppl for pkv
+# prompt_prefix_length = fineweb_prompt_prefix_length(
+#     PROMPT, tokenizer, language="svenska", prompt_type=prompt_type
+# )
+
+# Apply prompt template and save the result
+# logging.info(f"Applying prompts")
+# ds = ds.map(
+#    apply_askllm_prompt,
+#    batched=True,
+#    num_proc=16,
+#    fn_kwargs={
+#        "tokenizer": tokenizer,
+#        "language": args.language,
+#        "max_tokens": args.max_tokens,
+#    },
+# )
 
 
-ds = ds.map(word_len)
+ds = AskLLMDataset(ds, tokenizer, args.language, args.max_tokens, device)
 
-ds = ds.select(list(range(2000)))
-ds = ds.sort("word_count")
-
-# ds = ds.remove_columns(["id", "meta"])
-# Turn ds into a dataloader
-dataloader = torch.utils.data.DataLoader(ds, batch_size=8, num_workers=3)
-
-
-probs_450 = []
-text_dicts = []
+dataloader = torch.utils.data.DataLoader(
+    ds, batch_size=args.batch_size, num_workers=4, pin_memory=True
+)
 
 # Generate with dataloader
 for batch in tqdm(dataloader):
-    text_prompts = batch["text_prompt"]
-    model_inputs = tokenizer(
-        text_prompts,
-        return_tensors="pt",
-        padding=True,
-        truncation=True,
-        return_attention_mask=True,
-        add_special_tokens=True,
-    ).to(device)
-
-    generated_ids = model.generate(
-        **model_inputs,
+    outputs = model.generate(
+        **batch,
         max_new_tokens=1,
         do_sample=False,
         num_beams=1,
         output_logits=True,
         return_dict_in_generate=True,
     )
-    for i, logits in enumerate(generated_ids["logits"][0]):
-        decoded = tokenizer.decode(logits.argmax(-1))
-        softmax_score = softmax(logits, dim=-1)
-        probs_450.append(softmax_score[tokenizer.convert_tokens_to_ids("▁Yes")].to("cpu").item())
-        prob = softmax_score[tokenizer.convert_tokens_to_ids("▁Yes")].to("cpu").item()
-        text_dicts.append(
-            {
-                "text": " ".join(batch["text"][i].split(" ")[:450]),
-                # "text_prompt": batch["text_prompt"][i],
-                # "url": batch["url"][i],
-                # "timestamp": batch["timestamp"][i],
-                "prob": prob,
-            }
-        )
+    logits = outputs.logits
+    logger.debug(f"logits.shape: {logits.shape}")  # (batch_size, vocab_size)
+    # convert logits to probabilities
+    # see https://github.com/huggingface/transformers/blob/56baa03380fc17d85705240ebbc57c075a9b3f23/tests/generation/test_utils.py#L3507  # noqa: E501
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    logger.debug(f"probs.shape: {probs.shape}")  # (batch_size, vocab_size)
+    yes_probs = probs[:, yes_ids]
+    logger.debug(f"yes_probs.shape: {yes_probs.shape}")  # (batch_size, num_yes_tokens)
+    scores = torch.sum(yes_probs, dim=-1)
+    logger.debug(f"scores.shape: {scores.shape}")  # (batch_size,)
